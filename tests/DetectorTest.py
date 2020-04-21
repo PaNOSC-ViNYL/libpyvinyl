c574ae65db792ed54fa64c3dfe72b3afbe2a65a0
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2020-04-21 06:51:01 +0200

    Adding test skeletons.

---
+++ tests/DetectorTest.py
@@ -0,0 +1,42 @@
+import unittest
+
+from pyvinyl.Detector import Detector, DetectorParameters
+
+
+class DetectorTest(unittest.TestCase):
+    """
+    Test class for the Detector class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Setting up the test class. """
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Tearing down the test class. """
+        pass
+
+    def setUp(self):
+        """ Setting up a test. """
+        self.__files_to_remove = []
+        self.__dirs_to_remove = []
+
+    def tearDown(self):
+        """ Tearing down a test. """
+
+        for f in self.__files_to_remove:
+            if os.path.isfile(f): os.remove(f)
+        for d in self.__dirs_to_remove:
+            if os.path.isdir(d): shutil.rmtree(d)
+
+    def testDefaultConstruction(self):
+        """ Testing the default construction of the class. """
+
+        # Construct the object.
+        calculator = Detector()
+
+if __name__ == '__main__':
+    unittest.main()
+
