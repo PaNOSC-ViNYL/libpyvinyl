63b0bed4ffebb2c711f1cddb18cda88d1c3e9fa1
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2020-04-21 06:33:51 +0200

    Adding test suite.

---
+++ tests/test.py
@@ -0,0 +1,27 @@
+import unittest
+import os, sys
+
+
+import BaseCalculatorTest
+
+# Are we running on CI server?
+is_travisCI = ("TRAVIS_BUILD_DIR" in list(os.environ.keys())) and (os.environ["TRAVIS_BUILD_DIR"] != "")
+
+
+def suite():
+    suites = [
+             unittest.makeSuite(BaseCalculatorTest,               'test'),
+             ]
+
+    return unittest.TestSuite(suites)
+
+# Run the suite and return a success status code. This enables running an automated git-bisect.
+if __name__=="__main__":
+
+    result = unittest.TextTestRunner(verbosity=2).run(suite())
+
+    if result.wasSuccessful():
+        print('---> OK <---')
+        sys.exit(0)
+
+    sys.exit(1)
