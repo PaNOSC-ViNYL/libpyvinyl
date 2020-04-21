a16cee2585fb7d0e858e6cd4b55d8a7692d7018d
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2020-04-21 06:26:28 +0200

    Add funding acknowledgement.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pyvinyl - The python APIs for Virtual Neutron and x-raY Laboratory
+
+This project has received funding from the European Union's Horizon 2020 research and innovation programme under grant agreement No. 823852.
--- pyvinyl/BeamlinePropagator.py
+++ pyvinyl/BeamlinePropagator.py
@@ -32,3 +32,5 @@ class BeamlinePropagatorParameters(object, metaclass=ABCMeta):
 
 class BaseCalculator(object, metaclass=ABCMeta):
     pass
+
+# This project has received funding from the European Union's Horizon 2020 research and innovation programme under grant agreement No. 823852.
