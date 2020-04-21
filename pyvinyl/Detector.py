a7abe7b6b7795352cddf895a8bc610d7378cba84
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2020-04-21 06:16:01 +0200

    Basic class skeletons.

---
+++ pyvinyl/Detector.py
@@ -0,0 +1,34 @@
+"""
+:module BaseCalculator: Module hosting the BaseCalculator and DetectorParameters
+abstract classes.
+"""
+
+
+####################################################################################
+#                                                                                  #
+# This file is part of pyvinyl - The APIs for Virtual Neutron and x-raY            #
+# Laboratory.                                                                      #
+#                                                                                  #
+# Copyright (C) 2020  Carsten Fortmann-Grote                                       #
+#                                                                                  #
+# This program is free software: you can redistribute it and/or modify it under    #
+# the terms of the GNU Lesser General Public License as published by the Free      #
+# Software Foundation, either version 3 of the License, or (at your option) any    #
+# later version.                                                                   #
+#                                                                                  #
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY  #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A  #
+# PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details. #
+#                                                                                  #
+# You should have received a copy of the GNU Lesser General Public License along   #
+# with this program.  If not, see <https://www.gnu.org/licenses/                   #
+#                                                                                  #
+####################################################################################
+
+from abc import ABCMeta, abstractmethod
+
+class DetectorParameters(object, metaclass=ABCMeta):
+    pass
+
+class BaseCalculator(object, metaclass=ABCMeta):
+    pass
