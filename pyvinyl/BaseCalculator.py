82ef00daefc1b78a90fbf149a782e76e1d72ed77
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2020-04-25 09:53:52 +0200

    Implemented dump and reload.

--- pyvinyl/BaseCalculator.py
+++ pyvinyl/BaseCalculator.py
@@ -26,6 +26,11 @@ abstract classes.
 ####################################################################################
 
 from pyvinyl.AbstractBaseClass import AbstractBaseClass
+from abc import abstractmethod
+import copy
+from tempfile import mkstemp
+import os
+import dill
 
 class BaseParameters(AbstractBaseClass):
     """
@@ -33,10 +38,37 @@ class BaseParameters(AbstractBaseClass):
     Calculators.
     """
 
-    def __init__(self, **kwargs):
+    @abstractmethod
+    def __init__(self, filename=None, **kwargs):
         """
+        :param filename: If given, parameters will be loaded from file. File
+        must be in json format. See `serialize()` method for more details.
+
         :param kwargs: (key, value) pairs of parameters.
         """
+
+        for key, val in kwargs.items():
+
+            self.__dict__[key] = val
+
+    def __call__(self, **kwargs):
+        """ The copy constructor
+        :param kwargs: key-value pairs of parameters to change in the new
+        instance.
+
+        :return: A new parameters instance with optionally changed parameters.
+        """
+
+        new = copy.deepcopy(self)
+
+        if kwargs is None:
+            return new
+
+        for key, val in kwargs.items():
+            new.__dict__[key] = val
+
+        return new
+
 
 
 class BaseCalculator(AbstractBaseClass):
@@ -44,33 +76,120 @@ class BaseCalculator(AbstractBaseClass):
     :class BaseCalculator: Base class of all calculators.
     """
 
-    def __init__(self, parameters: BaseParameters, **kwargs):
+    @abstractmethod
+    def __init__(self, parameters=None, dumpfile=None, **kwargs):
         """
         :param parameters: The parameters for this calculator.
         :type  parameters: BaseParameters
+        
+        :param dumpfile: If given, load a previously dumped (aka pickled)
+        calculator. If both 'parameters' and 'dumpfile' are given, the dumpfile
+        is loaded first and parameters are overwritten. 
 
         :param kwargs: (key, value) pairs of further arguments to the
         calculator, e.g input_path, output_path.
         """
         
-        self.parameters = parameters
+        if parameters is None and dumpfile is None:
+            raise AttributeError("At least one of 'parameters' or 'dumpfile' must be given.")
+
+        if dumpfile is not None:
+            self.__load_from_dump(dumpfile)
+
+        if parameters is not None:
+            self.parameters = parameters
 
         if "output_path" in kwargs.keys():
             self.output_path = kwargs['output_path']
-        
+    
+    def __call__(self, parameters=None, **kwargs):
+        """ The copy constructor
+        :param parameters: The parameters for the new calculator.
+        :type  parameters: BaseParameters
+
+        :param kwargs: key-value pairs of parameters to change in the new
+        instance.
+
+        :return: A new parameters instance with optionally changed parameters.
+        """
+
+        new = copy.deepcopy(self)
+
+        for key, val in kwargs.items():
+            new.__dict__[key] = val
+
+        if parameters is not None:
+            new.parameters = parameters
+
+        return new
+    
+    def __load_from_dump(self, dumpfile):
+        """ """
+        """ Load a dill dump and initialize self's internals."""
+
+        with open(dumpfile, 'rb') as fhandle:
+            try:
+                tmp = dill.load(fhandle)
+            except:
+                raise IOError("Cannot load calculator from {}.".format(dumpfile)) 
+
+        self.__dict__ = copy.deepcopy(tmp.__dict__)
+
+        del tmp
+      
     @property
     def parameters(self):
         """ The parameters of this calculator. """
 
         return self.__parameters
+
     @parameters.setter
     def parameters(self, val):
 
-        if not isinstance(val, BaseParameters):
-            raise TypeError("""Passed argument 'parameters' has wrong type.
-            Expected BaseParameters, found {}.""".format(type(val)))
+        if not isinstance(val, (type(None), BaseParameters)):
+            raise TypeError("""Passed argument 'parameters' has wrong type.  Expected BaseParameters, found {}.""".format(type(val)))
 
         self.__parameters = val
-           
+
+    def dump(self, fname=None):
+        """
+        Dump class instance to file.
+
+        :param fname: Path to file to dump.
+
+        """
+
+        if fname is None:
+            _, fname = mkstemp(
+                    #suffix="_dump.dill",
+                    #prefix=self.__class__.__name__[-1],
+                    dir=os.getcwd(),
+                    )
+        try:
+            with open(fname, "wb") as file_handle:
+                dill.dump(self, file_handle)
+        except:
+            raise
+                # raise IOError("Cannot dump to file "+fname)
+
+        return fname
+
+# Mocks for testing. Have to be here to work around bug in dill that does not
+# like classes to be defined outside of __main__.
+class SpecializedParameters(BaseParameters):
+
+    def __init__(self, 
+                 photon_energy=None,
+                 pulse_energy=None,
+                 ):
+
+        super().__init__(photon_energy=photon_energy,
+                pulse_energy=pulse_energy)
+    
+class SpecializedCalculator(BaseCalculator):
+    def __init__(self, parameters=None, dumpfile=None, **kwargs):
+        
+        super().__init__(parameters, dumpfile, **kwargs)
+
 
 #This project has received funding from the European Union's Horizon 2020 research and innovation programme under grant agreement No. 823852.
--- tests/BaseCalculatorTest.py
+++ tests/BaseCalculatorTest.py
@@ -1,6 +1,8 @@
 import unittest
+import os
 
 from pyvinyl.BaseCalculator import BaseCalculator, BaseParameters
+from pyvinyl.BaseCalculator import SpecializedParameters, SpecializedCalculator
 from pyvinyl.AbstractBaseClass import AbstractBaseClass
 
 
@@ -12,12 +14,15 @@ class BaseCalculatorTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """ Setting up the test class. """
-        pass
+        cls.__default_parameters = SpecializedParameters(photon_energy=109.98,
+                pulse_energy=32.39)
+        cls.__default_calculator = SpecializedCalculator(cls.__default_parameters)
 
     @classmethod
     def tearDownClass(cls):
         """ Tearing down the test class. """
-        pass
+        del cls.__default_parameters
+        del cls.__default_calculator
 
     def setUp(self):
         """ Setting up a test. """
@@ -32,22 +37,128 @@ class BaseCalculatorTest(unittest.TestCase):
         for d in self.__dirs_to_remove:
             if os.path.isdir(d): shutil.rmtree(d)
 
+    def test_base_class_constructor_raises(self):
+        """ Test that we cannot construct instances of the base class. """
+
+        self.assertRaises(TypeError, BaseCalculator, self.__default_parameters)
+
     def test_default_construction(self):
         """ Testing the default construction of the class. """
 
         # Test positional arguments
-        self.assertRaises(TypeError, BaseCalculator)
+        self.assertRaises(AttributeError, SpecializedCalculator)
 
-        parameters = BaseParameters()
-        self.assertIsInstance(parameters, BaseParameters)
-        self.assertIsInstance(parameters, AbstractBaseClass)
+        calculator = SpecializedCalculator(self.__default_parameters)
 
-        calculator = BaseCalculator(parameters)
-
+        self.assertIsInstance(calculator, SpecializedCalculator)
         self.assertIsInstance(calculator, BaseCalculator)
         self.assertIsInstance(calculator, AbstractBaseClass)
 
-        self.assertRaises(TypeError, BaseCalculator, 1)
+        self.assertRaises(TypeError, SpecializedCalculator, 1)
+
+    def test_dump(self):
+        """ Test dumping to file. """
+        calculator = self.__default_calculator
+        
+        self.__files_to_remove.append(calculator.dump())
+        self.__files_to_remove.append(calculator.dump("dump.dill"))
+    
+    def test_resurrect_from_dump(self):
+        """ Test loading from dumpfile. """
+        calculator = self.__default_calculator
+
+        # dump
+        dump = calculator.dump()
+        self.__files_to_remove.append(dump)
+
+        del calculator
+
+        calculator = SpecializedCalculator(dumpfile=dump)
+        self.assertIsInstance(calculator, SpecializedCalculator)
+
+        self.assertEqual(calculator.parameters.photon_energy,
+                self.__default_parameters.photon_energy)
+
+
+class BaseParametersTest(unittest.TestCase):
+    """
+    Test class for the BaseCalculator class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Setting up the test class. """
+        cls.__default_parameters = SpecializedParameters(photon_energy=109.98,
+                pulse_energy=32.39)
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Tearing down the test class. """
+        del cls.__default_parameters
+
+    def setUp(self):
+        """ Setting up a test. """
+        self.__files_to_remove = []
+        self.__dirs_to_remove = []
+
+    def tearDown(self):
+        """ Tearing down a test. """
+
+        for f in self.__files_to_remove:
+            if os.path.isfile(f): os.remove(f)
+        for d in self.__dirs_to_remove:
+            if os.path.isdir(d): shutil.rmtree(d)
+
+    def test_base_class_constructor_raises(self):
+        """ Test that we cannot construct instances of the base class. """
+
+        self.assertRaises(TypeError, BaseParameters)
+
+    def test_default_construction(self):
+        """ Testing the default construction of the class. """
+
+        # Test positional arguments
+        self.assertRaises(TypeError, SpecializedCalculator)
+
+        parameters = SpecializedParameters()
+        self.assertIsInstance(parameters, SpecializedParameters)
+        self.assertIsInstance(parameters, BaseParameters)
+        self.assertIsInstance(parameters, AbstractBaseClass)
+
+    def test_copy_construction(self):
+        """ Test the copy constructor behaves as expected. """
+
+        parameters = self.__default_parameters
+
+        new_parameters = parameters(photon_energy=10.00)
+
+        self.assertIsInstance(new_parameters, SpecializedParameters)
+        self.assertIsInstance(new_parameters, BaseParameters)
+        self.assertIsInstance(new_parameters, AbstractBaseClass)
+        self.assertEqual(new_parameters.photon_energy, 10.00)
+
+    def test_copy_construct_calculator(self):
+        """ Test the copy constructor behaves as expected. """
+
+        new_parameters = self.__default_parameters(photon_energy=10.00)
+        new_calculator = self.__default_calculator(parameters=new_parameters)
+        self.assertIsInstance(new_calculator, SpecializedCalculator)
+        self.assertIsInstance(new_calculator, BaseCalculator)
+        self.assertIsInstance(new_calculator, AbstractBaseClass)
+        self.assertEqual(new_calculator.parameters.photon_energy, 10.00)
+
+        new_calculator_2 = new_calculator(parameters=self.__default_parameters(pulse_energy=34.87))
+        self.assertEqual(new_calculator_2.parameters.photon_energy, 109.98)
+        self.assertEqual(new_calculator_2.parameters.pulse_energy, 34.87)
+    
+    def test_serialize(self):
+        """ Test serialization of parameters. """
+        self.assertFalse(True)
+
+    def test_unserialize(self):
+        """ Test loading of parameters from serialized. """
+        self.assertFalse(True)
+    
 
 if __name__ == '__main__':
     unittest.main()
