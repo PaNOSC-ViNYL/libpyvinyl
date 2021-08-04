00af95ea8ee8a09d08d2e877d9a742725dae7e04
Juncheng E <juncheng.e@xfel.eu>
2021-08-04 23:41:23 +0200

    implement the parameters class from Mads

--- libpyvinyl/BaseCalculator.py
+++ libpyvinyl/BaseCalculator.py
@@ -3,7 +3,6 @@
 
 """
 
-
 ####################################################################################
 #                                                                                  #
 # This file is part of libpyvinyl - The APIs for Virtual Neutron and x-raY            #
@@ -26,90 +25,27 @@
 ####################################################################################
 
 from abc import abstractmethod
-from jsons import JsonSerializable
-from numbers import Number
-from numpy import ndarray
 from libpyvinyl.AbstractBaseClass import AbstractBaseClass
 from tempfile import mkstemp
 import copy
 import dill
 import h5py
-import json, jsons
+import sys
 import logging
 import numpy
 import os
+from .Parameters import Parameters
 
-logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.WARNING)
-       
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',
+                    level=logging.WARNING)
 
-class Parameters(JsonSerializable, AbstractBaseClass):
-    """
-
-    :class Parameters: Base class to encapsulate all parametrizations of Calculators.
-
-    """
-
-    def __init__(self, **kwargs):
-        """
-
-        :param kwargs: (key, value) pairs of parameters.
-
-        """
-
-        # Update parameters with additionaly given arguments.
-        self.__dict__.update(kwargs)
-
-    def __call__(self, **kwargs):
-        """ The copy constructor
-
-        :param kwargs: key-value pairs of parameters to change in the new
-        instance.
-
-        :return: A new parameters instance with optionally changed parameters.
-
-        """
-
-        new = copy.deepcopy(self)
-
-        if kwargs is None:
-            return new
-
-        new.__dict__.update(kwargs)
-
-        return new
-    
-    @classmethod
-    def from_json(self, fname:str):
-        """ 
-        Initialize an instance from a json file. 
-        
-        :param fname: The filename (path) of the json file.
-        :type  fname: str
-
-        """
-        with open(fname, 'r') as fp:
-            instance = self.load(json.load(fp))
-        
-        return instance
-
-    def to_json(self, fname:str):
-        """ 
-        Save this parameters class to a human readable json file.
-
-        :param fname: Write to this file.
-        :type  fname: str
-
-        """
-
-        with open(fname, 'w') as fp:
-            json.dump(self.dump(), fp)
 
 class BaseCalculator(AbstractBaseClass):
     """
 
     :class BaseCalculator: Base class of all calculators.
-    
-    This class is provides the libpyvinyl API. It defines all methods 
+
+    This class is provides the libpyvinyl API. It defines all methods
     through which a user interacts with the simulation backengines.
 
     This class is to be used as a base class for all calculators that implement
@@ -118,14 +54,13 @@ class BaseCalculator(AbstractBaseClass):
     backengine as all other ViNyL Calculators.
 
     """
-
     @abstractmethod
     def __init__(self, parameters=None, dumpfile=None, **kwargs):
         """
-        
+
         :param parameters: The parameters for this calculator.
         :type  parameters: Parameters
-        
+
         :param dumpfile: If given, load a previously dumped (aka pickled) calculator.
 
         :param kwargs: (key, value) pairs of further arguments to the calculator, e.g input_path, output_path.
@@ -138,7 +73,7 @@ class BaseCalculator(AbstractBaseClass):
         ```
         # Define a specialized calculator.
         class MyCalculator(BaseCalculator):
-            
+
             def __init__(self, parameters=None, dumpfile=None, **kwargs):
                 super()__init__(parameters, dumpfile, **kwargs)
 
@@ -160,12 +95,13 @@ class BaseCalculator(AbstractBaseClass):
         ```
 
         """
-        
+
         # Set data
         self.__data = None
 
         if parameters is None and dumpfile is None:
-            raise AttributeError("At least one of 'parameters' or 'dumpfile' must be given.")
+            raise AttributeError(
+                "At least one of 'parameters' or 'dumpfile' must be given.")
 
         if dumpfile is not None:
             self.__load_from_dump(dumpfile)
@@ -175,7 +111,6 @@ class BaseCalculator(AbstractBaseClass):
 
         if "output_path" in kwargs:
             self.output_path = kwargs["output_path"]
-        
 
     def __call__(self, parameters=None, **kwargs):
         """ The copy constructor
@@ -197,24 +132,25 @@ class BaseCalculator(AbstractBaseClass):
             new.parameters = parameters
 
         return new
-    
+
     def __load_from_dump(self, dumpfile):
         """ """
-        """ 
+        """
         Load a dill dump and initialize self's internals.
-        
+
         """
 
         with open(dumpfile, 'rb') as fhandle:
             try:
                 tmp = dill.load(fhandle)
             except:
-                raise IOError("Cannot load calculator from {}.".format(dumpfile)) 
+                raise IOError(
+                    "Cannot load calculator from {}.".format(dumpfile))
 
         self.__dict__ = copy.deepcopy(tmp.__dict__)
 
         del tmp
-      
+
     @property
     def parameters(self):
         """ The parameters of this calculator. """
@@ -225,7 +161,9 @@ class BaseCalculator(AbstractBaseClass):
     def parameters(self, val):
 
         if not isinstance(val, (type(None), Parameters)):
-            raise TypeError("""Passed argument 'parameters' has wrong type. Expected Parameters, found {}.""".format(type(val)))
+            raise TypeError(
+                """Passed argument 'parameters' has wrong type. Expected Parameters, found {}."""
+                .format(type(val)))
 
         self.__parameters = val
 
@@ -239,10 +177,9 @@ class BaseCalculator(AbstractBaseClass):
 
         if fname is None:
             _, fname = mkstemp(
-                    #suffix="_dump.dill",
-                    #prefix=self.__class__.__name__[-1],
-                    dir=os.getcwd(),
-                    )
+                #suffix="_dump.dill",
+                #prefix=self.__class__.__name__[-1],
+                dir=os.getcwd(), )
         try:
             with open(fname, "wb") as file_handle:
                 dill.dump(self, file_handle)
@@ -252,7 +189,7 @@ class BaseCalculator(AbstractBaseClass):
         return fname
 
     @abstractmethod
-    def saveH5(self, fname:str, openpmd:bool=True):
+    def saveH5(self, fname: str, openpmd: bool = True):
         """ Save the simulation data to hdf5 file.
 
         :param fname: The filename (path) of the file to write the data to.
@@ -265,10 +202,11 @@ class BaseCalculator(AbstractBaseClass):
     @property
     def data(self):
         return self.__data
+
     @data.setter
     def data(self, val):
         raise AttributeError("Attribute 'data' is read-only.")
-    
+
     @abstractmethod
     def backengine(self):
         pass
@@ -283,7 +221,7 @@ class BaseCalculator(AbstractBaseClass):
         """
         if len(sys.argv) == 2:
             fname = sys.argv[1]
-            calculator=cls(fname)
+            calculator = cls(fname)
             status = calculator._run()
             sys.exit(status)
 
@@ -294,19 +232,19 @@ class BaseCalculator(AbstractBaseClass):
         :return: status code.
 
         """
-        result=self.backengine()
+        result = self.backengine()
 
         if result is None:
-            result=0
+            result = 0
 
         return result
 
     def _set_data(self, data):
         """ """
-        """ Private method to store the data on the object. 
+        """ Private method to store the data on the object.
 
         :param data: The data to store.
-        
+
         """
 
         self.__data = data
@@ -314,37 +252,37 @@ class BaseCalculator(AbstractBaseClass):
 
 # Mocks for testing. Have to be here to work around bug in dill that does not
 # like classes to be defined outside of __main__.
-class SpecializedParameters(Parameters):
-
-    def __init__(
-            self, 
-            photon_energy:float,
-            pulse_energy:float,
-            **kwargs
-            ):
-        
-        super().__init__(
-                photon_energy=photon_energy,
-                pulse_energy=pulse_energy,
-                **kwargs,
-                )
-    
 class SpecializedCalculator(BaseCalculator):
     def __init__(self, parameters=None, dumpfile=None, **kwargs):
-        
+
         super().__init__(parameters, dumpfile, **kwargs)
 
+    def setParams(self, photon_energy: float = 10, pulse_energy: float = 1e-3):
+        if not isinstance(self.parameters, Parameters):
+            self.parameters = Parameters()
+        self.parameters.new_parameter("photon_energy",
+                                      unit="keV",
+                                      comment="Photon energy")
+        self.parameters['photon_energy'].set_value(photon_energy)
+
+        self.parameters.new_parameter("pulse_energy",
+                                      unit="joule",
+                                      comment="Pulse energy")
+        self.parameters['pulse_energy'].set_value(pulse_energy)
+
     def backengine(self):
-        self._BaseCalculator__data = numpy.random.normal(loc=self.parameters.photon_energy,
-                scale=0.001*self.parameters.photon_energy,
-                size=(100,)
-                )
-            
+        self._BaseCalculator__data = numpy.random.normal(
+            loc=self.parameters['photon_energy'],
+            scale=0.001 * self.parameters['photon_energy'],
+            size=(100, ))
+
         return 0
 
     def saveH5(self, openpmd=False):
         with h5py.File(self.output_path, "w") as h5:
-            ds = h5.create_dataset("/data", data=self.data)    
-            
+            ds = h5.create_dataset("/data", data=self.data)
+
             h5.close()
+
+
 #This project has received funding from the European Union's Horizon 2020 research and innovation programme under grant agreement No. 823852.
---
+++ tests/ParametersTest.py
@@ -0,0 +1,177 @@
+import unittest
+import os
+import tempfile
+
+from libpyvinyl.Parameters import Parameter
+from libpyvinyl.Parameters import Parameters
+from libpyvinyl.Parameters import ParametersCollection
+
+
+class Test_Parameter(unittest.TestCase):
+    def test_initialize_parameter_simple(self):
+        par = Parameter("test")
+        self.assertEqual(par.name, "test")
+
+    def test_initialize_parameter_complex(self):
+        par = Parameter("test", unit="cm", comment="comment string")
+        self.assertEqual(par.name, "test")
+        self.assertEqual(par.unit, "cm")
+        self.assertEqual(par.comment, "comment string")
+
+    def test_parameter_legal_interval(self):
+        par = Parameter("test")
+        par.add_legal_interval(3, 4.5)
+
+        self.assertTrue(par.is_legal(3.5))
+        self.assertFalse(par.is_legal(1.0))
+
+    def test_parameter_illegal_interval(self):
+        par = Parameter("test")
+        par.add_illegal_interval(3, 4.5)
+
+        self.assertFalse(par.is_legal(3.5))
+        self.assertTrue(par.is_legal(1.0))
+
+    def test_parameter_multiple_intervals(self):
+        par = Parameter("test")
+        par.add_legal_interval(None, 8.5)  # minus infinite to 8.5
+        par.add_illegal_interval(3, 4.5)
+
+        self.assertTrue(par.is_legal(-831.0))
+        self.assertFalse(par.is_legal(3.5))
+        self.assertTrue(par.is_legal(5.0))
+        self.assertFalse(par.is_legal(10.0))
+
+    def test_parameter_option(self):
+        par = Parameter("test")
+        par.add_option(9.8)
+        par.add_option(True)
+        par.add_option(["A", 38])
+
+        self.assertFalse(par.is_legal(10))
+        self.assertTrue(par.is_legal(9.8))
+        self.assertTrue(par.is_legal(True))
+        self.assertTrue(par.is_legal("A"))
+        self.assertTrue(par.is_legal(38))
+
+    def test_parameter_set_value(self):
+        par = Parameter("test")
+        par.add_legal_interval(3, 4.5)
+
+        par.set_value(4.0)
+        self.assertEqual(par.value, 4.0)
+
+        par.set_value(5.0)  # Since this is not allowed, will be ignored
+        self.assertEqual(par.value, 4.0)
+
+    def test_parameter_from_dict(self):
+        par = Parameter("test")
+        par.add_legal_interval(3, 4.5)
+        par.set_value(4.0)
+        par_from_dict = Parameter.from_dict(par.__dict__)
+        self.assertEqual(par_from_dict.value, 4.0)
+
+
+class Test_Parameters(unittest.TestCase):
+    def test_initialize_parameters_from_list(self):
+        par1 = Parameter("test")
+        par1.set_value(8)
+        par2 = Parameter("test2", unit="meV")
+
+        parameters = Parameters([par1, par2])
+
+        self.assertEqual(parameters["test"].value, 8)
+
+    def test_initialize_parameters_from_add(self):
+        par1 = Parameter("test")
+        par1.set_value(8)
+        par2 = Parameter("test2", unit="meV")
+        par2.set_value(10)
+
+        parameters = Parameters()
+        parameters.add(par1)
+        parameters.add(par2)
+
+        self.assertEqual(parameters["test"].value, 8)
+        self.assertEqual(parameters["test2"].value, 10)
+
+    def test_json(self):
+        par1 = Parameter("test")
+        par1.set_value(8)
+        par2 = Parameter("test2", unit="meV")
+        par2.set_value(10)
+
+        parameters = Parameters()
+        parameters.add(par1)
+        parameters.add(par2)
+        with tempfile.TemporaryDirectory() as d:
+            # tmp_file = os.path.join(d, 'test.json')
+            tmp_file = 'test.json'
+            parameters.to_json(tmp_file)
+            params_json = Parameters.from_json(tmp_file)
+            self.assertEqual(params_json['test2'].value, 10)
+
+
+def source_calculator():
+    """
+    Little dummy calculator that sets up a parameters object for a source
+    """
+    parameters = Parameters()
+    parameters.new_parameter("energy",
+                             unit="eV",
+                             comment="Source energy setting")
+    parameters["energy"].add_legal_interval(0, 1E6)
+
+    parameters.new_parameter("delta_energy",
+                             unit="eV",
+                             comment="Energy spread fwhm")
+    parameters["delta_energy"].add_legal_interval(0, 400)
+
+    parameters.new_parameter("position", unit="cm", comment="Source center")
+    parameters["position"].add_legal_interval(-1.5, 1.5)
+
+    parameters.new_parameter("gaussian",
+                             comment="False for flat, True for gaussian")
+    parameters["gaussian"].add_option([False, True])
+
+    return parameters
+
+
+def sample_calculator():
+    """
+    Little dummy calculator that sets up a parameters object for a sample
+    """
+    parameters = Parameters()
+    parameters.new_parameter("radius", unit="cm", comment="Sample radius")
+    parameters["radius"].add_legal_interval(0, None)  # To infinite
+
+    parameters.new_parameter("height", unit="cm", comment="Sample height")
+    parameters["height"].add_legal_interval(0, None)
+
+    parameters.new_parameter("absorption",
+                             unit="barns",
+                             comment="absorption cross section")
+    parameters["absorption"].add_legal_interval(0, None)
+
+    return parameters
+
+
+class Test_Instruments(unittest.TestCase):
+    def setUp(self):
+        # We start creating our instrument with a ParametersCollection
+        self.instr_parameters = ParametersCollection()
+
+        # We insert a source and get some parameters out
+        source_pars = source_calculator()
+        # These are added to the instr_parameters so they can be controlled
+        self.instr_parameters.add("Source", source_pars)
+
+        # We also add a few sample objects with their parameter objects
+        top_sample_pars = sample_calculator()
+        self.instr_parameters.add("Sample top", top_sample_pars)
+
+        bottom_sample_pars = sample_calculator()
+        self.instr_parameters.add("Sample bottom", bottom_sample_pars)
+
+    def test_json(self):
+        self.instr_parameters.to_json('instrument.json')
