0d0ccadb82207502b54a3a9ba73c4fce8dbd190e
Carsten Fortmann-Grote <grotec@evolbio.mpg.de>
2021-12-03 11:26:09 +0100

    Small cosmetic fixes.

--- tests/Test.py
+++ tests/Test.py
@@ -5,12 +5,12 @@
 
 ####################################################################################
 #                                                                                  #
-# This file is part of libpyvinyl - The APIs for Virtual Neutron and x-raY            #
+# This file is part of libpyvinyl - The APIs for Virtual Neutron and x-raY         #
 # Laboratory.                                                                      #
 #                                                                                  #
 # Copyright (C) 2020  Carsten Fortmann-Grote                                       #
 #                                                                                  #
-# This program is free software: you can redistribute it and/or modify it under    #
+# This program is free software: you can redistribute it and/or modify it under   #
 # the terms of the GNU Lesser General Public License as published by the Free      #
 # Software Foundation, either version 3 of the License, or (at your option) any    #
 # later version.                                                                   #
