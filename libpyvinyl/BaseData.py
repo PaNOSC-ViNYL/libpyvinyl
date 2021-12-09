5e4cc03cda7060e7c6df6af94ab56246efb5b52f
Juncheng E <juncheng.e@xfel.eu>
2021-12-09 16:04:37 +0100

    update BaseData, BaseFormat examples, add BaseDataTest.py

--- libpyvinyl/BaseData.py
+++ libpyvinyl/BaseData.py
@@ -1,58 +1,78 @@
+""" :module BaseData: Module hosts the BaseData class."""
 from abc import abstractmethod
 from libpyvinyl.AbstractBaseClass import AbstractBaseClass
-from importlib import import_module
 
 
 class BaseData(AbstractBaseClass):
+    # It represents a python object
     """The abstract wrapper data class. It's designed in a way so that its inheritance is an API of a
     kind of data (e.g. wavefront, sample, and diffraction) rather than that of a certain data format."""
-    @abstractmethod
-    def __init__(self):
-        self._ioformats = {
-            'UNKNOWN': {  # FORMAT KEY
-                'description': 'UNKNOWN data format',  # FORMAT DISCRIPTION
-                'ext': '',  # FORMAT EXTENSION
-                'module': '',  # API MODULE NAME OF THE FORMAT
-                'format_class': '',  # API MODULE NAME OF THE FORMAT
-                'read_kwargs': [''],  # KEYWORDS LIST NEEDED TO READ
-                'write_kwargs': ['']  # KEYWORDS LIST NEEDED TO WRITE
-            }
-        }
+    def __init__(self,
+                 key,
+                 expected_data,
+                 data_dict=None,
+                 filename=None,
+                 file_format_class=None,
+                 file_format_kwargs=None):
+        self.__key = key
+        self.__expected_data = expected_data
+
+        # This will be always be None if the data class is mapped to a file
+        self.__data_dict = data_dict
+        # These will be always be None if the data class is mapped to a python data dict object
+        self.__filename = filename
+        self.__file_format_class = file_format_class
+        self.__file_format_kwargs = file_format_kwargs
 
     @property
-    def ioformats(self):
-        return self._ioformats
+    def key(self):
+        """The key of the class instance for calculator usage"""
+        return self.__key
+
+    @property
+    def mapping_type(self):
+        if self.__data_dict is not None:
+            return 'Python Dictionary'
+        elif self.__filename is not None:
+            return 'Data file: {}'.format(self.__filename)
+        else:
+            raise TypeError(
+                'Niether self.__data_dict or self.__filename was found.')
 
     @staticmethod
-    def _add_ioformat(ioformats,
-                      key: str,
-                      description: str,
-                      ext: str,
-                      module: str,
-                      format_class: str,
-                      read_kwargs=[''],
-                      write_kwargs=['']):
-        ioformats[key] = {}
-        ioformats[key]['description'] = description
-        ioformats[key]['ext'] = ext
-        ioformats[key]['module'] = module
-        ioformats[key]['format_class'] = format_class
-        ioformats[key]['read_kwargs'] = read_kwargs
-        ioformats[key]['write_kwargs'] = write_kwargs
+    def _add_ioformat(format_dict, format_class):
+        register = format_class.format_register()
+        for key, val in register.items():
+            if key == 'key':
+                this_format = val
+                format_dict[val] = {}
+            else:
+                format_dict[this_format][key] = val
 
-    def listFormats(self):
+    @classmethod
+    @abstractmethod
+    def supported_formats(self):
+        format_dict = {}
+        # Add the suppoted format classes when creating a concrete class.
+        # See the example at xx
+        self.__add_ioformat(format_dict, FormatClass)
+        return format_dict
+
+    @classmethod
+    def list_formats(self):
         """Print supported formats"""
         out_string = ''
-        for key in self.ioformats:
-            dicts = self.ioformats[key]
+        supported_formats = self.supported_formats()
+        for key in supported_formats:
+            dicts = supported_formats[key]
             out_string += f'Key: {key}\n'
             out_string += 'Description: {}\n'.format(dicts['description'])
             ext = dicts['ext']
             if ext != '':
                 out_string += 'File extension: {}\n'.format(ext)
-            module = dicts['module']
-            if module != '':
-                out_string += 'API module: {}\n'.format(module)
+            format_class = dicts['format_class']
+            if format_class != '':
+                out_string += 'Format class: {}\n'.format(format_class)
             kwargs = dicts['read_kwargs']
             if kwargs != ['']:
                 out_string += 'Extra reading keywords: {}\n'.format(kwargs)
@@ -63,32 +83,105 @@ class BaseData(AbstractBaseClass):
         print(out_string)
 
     @classmethod
-    def read(self, filename: str, format_class, **kwargs):
-        """Read the data from the file with the `filename` in the `format`. It returns an instance of the data class"""
-        # module_name = self._ioformats[format]['module']
-        # format_class = self._ioformats[format]['format_class']
-        # data_class = getattr(import_module(module_name), format_class)
-        ## This should work if read is a classmethod
-        return format_class.read(filename, **kwargs)
-
-    def write(self, filename: str, format_class, **kwargs):
-        """Save the data with the `filename` in the `format`."""
-        return format_class.write(self, filename, **kwargs)
+    def from_file(cls, filename: str, format_class, key, **kwargs):
+        """Create the data class by the file in the `format`."""
+        return cls(key,
+                   filename=filename,
+                   file_format_class=format_class,
+                   file_format_kwargs=kwargs)
 
     @classmethod
-    def convert(self, input_filename, input_format, output_filename,
-                output_format, **kwargs):
-        """Convert a file into another format"""
-        module_name = self._ioformats[input_format]['module']
-        data_module = import_module(module_name)
-        # If there is a direct converter
-        if data_module.convert(input_filename, output_filename, output_format,
-                               **kwargs):
-            pass
-        else:
-            data_obj = self.read(input_filename, input_format)
-            out_module_name = self._ioformats[input_format]['module']
-            out_data_module = import_module(out_module_name)
-            out_data_module.write(data_obj, output_filename, **kwargs)
+    def from_dict(cls, data_dict, key):
+        """Create the data class by a python dictionary."""
+        return cls(key, data_dict=data_dict)
+
+    def write(self, filename: str, format_class, key=None, **kwargs):
+        # From either a file or a python object to a file
+        # The behaviour related to a file will always be handled by
+        # the format class.
+        """Save the data with the `filename` in the `format`."""
+        # If it's a python dictionary mapping, write with the specified format_class
+        # directly.
+        if self.mapping_type == 'Python Dictionary':
+            return format_class.write(self, filename, key, **kwargs)
+        elif format_class in self.__file_format_class.direct_convert_formats():
+            return self.__file_format_class.convert(self.__filename, filename,
+                                                    format_class, key,
+                                                    **kwargs)
+        # If it's a file mapping and would like to write in the same fileformat of the
+        # mapping, it will let the user know that a file containing the data in the same format already existed.
+        elif format_class == self.__file_format_class:
+            print(
+                f"Hint: This data already existed in the file {self.__filename} in format {self.__file_format_class}. `cp {self.__filename} {filename}` could be faster."
+            )
+            print(
+                f"Will still write the data into the file {filename} in format {format_class}"
+            )
+        # The default behaviour is to read the data into a dict and write it to a file.
+        return format_class.write(self, filename, key, **kwargs)
+
+    def get_dict_data(self):
+        # python object to python object
+        if self.__data_dict is not None:
+            data_to_return = self.__expected_data.copy()
+            # It will automatically check the data needed to be extracted.
+            for key in data_to_return.keys():
+                data_to_return[key] = self.__data_dict[key]
+            return data_to_return
+
+    def get_file_data(self):
+        # file to python object
+        if self.__filename is not None:
+            data_to_return = self.__expected_data.copy()
+            data_to_read = self.__file_format_class.read(
+                self.__filename, **self.__file_format_kwargs)
+            # It will automatically check the data needed to be extracted.
+            for key in data_to_return.keys():
+                data_to_return[key] = data_to_read[key]
+            return data_to_return
+
+    def get_data(self):
+        """Return the data in a dictionary"""
+        # From either a file or a python object to a python object
+        if self.__data_dict is not None:
+            return self.get_dict_data()
+        elif self.__filename is not None:
+            return self.get_file_data()
+
+    def __repr__(self):
+        """Returns strings of Data objets info"""
+        string = ''
+        string += self.key + ': ' + self.mapping_type + '\n'
+        return string
 
 
+# DataCollection class
+class DataCollection():
+    def __init__(self, *args):
+        self.data_object_dict = {}
+        self.add_data(*args)
+
+    def __getitem__(self, *keys):
+        subset = []
+        for key in keys:
+            subset.append(self.data_object_dict[key])
+        return DataCollection(*subset)
+
+    def add_data(self, *args):
+        """Add data objects to the data colletion"""
+        for data in args:
+            self.data_object_dict[data.key] = data
+
+    def get_data_object(self, key):
+        return self.data_object_dict[key]
+
+    def to_list(self):
+        """Export a list of the data objects in the data colletion"""
+        return [self.data_object_dict[key] for key in self.data_dict.keys()]
+
+    def __repr__(self):
+        """Returns strings of Data objets info"""
+        string = ''
+        for data_object in self.data_object_dict.values():
+            string += str(data_object)
+        return string
--- libpyvinyl/BaseFormat.py
+++ libpyvinyl/BaseFormat.py
@@ -1,47 +1,250 @@
 from abc import abstractmethod
 from libpyvinyl.AbstractBaseClass import AbstractBaseClass
-from importlib import import_module
 from libpyvinyl.BaseData import BaseData
+import numpy as np
+
 
 class BaseFormat(AbstractBaseClass):
     """The abstract format class. It's the interface of a certain data format."""
-    @abstractmethod
     def __init__(self):
-        self.__format_register = {
-            'key': 'Base',  # FORMAT KEY
-            'description': 'Base data format',  # FORMAT DISCRIPTION
-            'ext': 'base',  # FORMAT EXTENSION
-            'module':
-            'libpyvinyl.BaseData.BaseFormat',  # API MODULE NAM OF THE FORMAT
-            'read_kwargs': [''],  # KEYWORDS LIST NEEDED TO READ
-            'write_kwargs': ['']  # KEYWORDS LIST NEEDED TO WRITE
-        }
-        self.__direct_convert_formats = ['format_key_01', 'format_key_02']
+        # This class is not designed in a object-oriented model, rather a collection of functions.
+        pass
 
-    @property
+    @classmethod
+    @abstractmethod
     def format_register(self):
-        return self.__format_register
+        # Redefine this `format_register` method for a concrete format class.
+        key = 'Base'
+        desciption = 'Base data format'
+        file_extension = 'base'
+        read_kwargs = ['']
+        write_kwargs = ['']
+        return self._create_format_register(key, desciption, file_extension,
+                                            read_kwargs, write_kwargs)
+
+    @staticmethod
+    @abstractmethod
+    def direct_convert_formats():
+        # Assume the format can be converted directly to the formats supported by these classes:
+        # AFormat, BFormat
+        # Redefine this `direct_convert_formats` for a concrete format class
+        return [Aformat, BFormat]
+
+    @classmethod
+    def _create_format_register(cls,
+                                key: str,
+                                desciption: str,
+                                file_extension: str,
+                                read_kwargs=[''],
+                                write_kwargs=['']):
+        format_register = {
+            'key': key,  # FORMAT KEY
+            'description': desciption,  # FORMAT DISCRIPTION
+            'ext': file_extension,  # FORMAT EXTENSION
+            'format_class': cls,  # CLASS NAME OF THE FORMAT
+            'read_kwargs': read_kwargs,  # KEYWORDS LIST NEEDED TO READ
+            'write_kwargs': write_kwargs  # KEYWORDS LIST NEEDED TO WRITE
+        }
+        return format_register
 
     @classmethod
     @abstractmethod
-    def read(self, filename: str, **kwargs) -> BaseData:
-        """Read the data from the file with the `filename`. It returns an instance of the data class"""
-        raise NotImplementedError()
+    def read(self, filename: str, **kwargs) -> dict:
+        """Read the data from the file with the `filename` to a dictionary. The dictionary will
+        be used by its corresponding data class."""
+        # Example codes. Redefine this function in a concrete class.
+        data_dict = {}
+        with h5py.File(filename, 'r') as h5:
+            for key, val in h5.items():
+                data_dict[key] = val[()]
+        return data_dict
 
     @classmethod
     @abstractmethod
-    def write(self, object: BaseData, filename: str, **kwargs):
+    def write(self, object: BaseData, filename: str, key: str, **kwargs):
         """Save the data with the `filename`."""
-
-        raise NotImplementedError()
-        
+        # Example codes. Redefine this function in a concrete class.
+        obj_key = key
+        with h5py.File(filename, 'w') as h5:
+            for key, val in object.get_data().items():
+                h5[key] = val
+        return object.from_file(filename, BaseData, obj_key)
 
     @classmethod
     @abstractmethod
-    def convert(self, input: str, output: str, output_format: str, **kwargs):
+    def convert(self, input: str, output: str, output_format_class: str, key,
+                **kwargs):
         """Direct convert method, if the default converting would be too slow or not suitable for the output_format"""
-        if output_format in self.__direct_convert_formats:
-            # self.format_convert(input, input, output_format)
-            return True
+        # If there is no direct converting supported:
+        raise NotImplementedError
+        if output_format_class is AFormat:
+            return self.convert_to_AFormat(input, output, key)
+
+
+class ExampleData(BaseData):
+    """The simplest data class example"""
+    def __init__(self,
+                 key,
+                 data_dict=None,
+                 filename=None,
+                 file_format_class=None,
+                 file_format_kwargs=None):
+        self.__expected_data = {}
+        self.__expected_data['arr'] = np.zeros((1, 2))
+        self.__expected_data['number'] = 1
+
+        super().__init__(key,
+                         self.__expected_data,
+                         data_dict=data_dict,
+                         filename=filename,
+                         file_format_class=file_format_class,
+                         file_format_kwargs=file_format_kwargs)
+
+    @classmethod
+    def supported_formats(self):
+        format_dict = {}
+        # Add the suppoted format classes when creating a concrete class.
+        self._add_ioformat(format_dict, ExampleAFormat)
+        self._add_ioformat(format_dict, ExampleBFormat)
+        return format_dict
+
+
+class ExampleAFormat(BaseFormat):
+    """A format class of Example A"""
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def format_register(self):
+        # Redefine this `format_register` method for a concrete format class.
+        key = 'Example_A'
+        desciption = 'Example A data format'
+        file_extension = '.npz'
+        read_kwargs = ['']
+        write_kwargs = ['']
+        return self._create_format_register(key, desciption, file_extension,
+                                            read_kwargs, write_kwargs)
+
+    @staticmethod
+    def direct_convert_formats():
+        # Assume the format can be converted directly to the formats supported by these classes:
+        # AFormat, BFormat
+        # Redefine this `direct_convert_formats` for a concrete format class
+        return [ExampleBFormat]
+
+    @classmethod
+    def read(cls, filename: str) -> dict:
+        """Read the data from the file with the `filename` to a dictionary. The dictionary will
+        be used by its corresponding data class."""
+        data_dict = np.load(filename)
+        return data_dict
+
+    @classmethod
+    def write(cls, object: ExampleData, filename: str, key: str = None):
+        """Save the data with the `filename`."""
+        data_dict = object.get_data()
+        arr = data_dict['arr']
+        number = data_dict['number']
+        np.savez(filename, arr=arr, number=number)
+        if key is not None:
+            return object.from_file(filename, cls, key)
+
+    @classmethod
+    def convert(cls,
+                input: str,
+                output: str,
+                output_format_class: str,
+                key=None,
+                **kwargs):
+        """Direct convert method, if the default converting would be too slow or not suitable for the output_format"""
+        if output_format_class is ExampleBFormat:
+            cls.convert_to_ExampleBFormat(input, output)
         else:
-            return False
+            raise TypeError(
+                "Unsupported format {}".format(output_format_class))
+        if key is not None:
+            return ExampleData.from_file(output, output_format_class, key)
+
+    @classmethod
+    def convert_to_ExampleBFormat(cls, input: str, output: str):
+        """The engine of convert method."""
+        print("Converting ExampleA to ExampleB")
+        data_dict = np.load(input)
+        data = np.array([data_dict['arr'], np.zeros_like(data_dict['arr'])])
+        data[1, :] = data_dict['number']
+        np.save(output, data)
+
+
+class ExampleBFormat(BaseFormat):
+    """A format class of Example B"""
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def format_register(self):
+        # Redefine this `format_register` method for a concrete format class.
+        key = 'Example_B'
+        desciption = 'Example B data format'
+        file_extension = '.npy'
+        read_kwargs = ['']
+        write_kwargs = ['']
+        return self._create_format_register(key, desciption, file_extension,
+                                            read_kwargs, write_kwargs)
+
+    @staticmethod
+    def direct_convert_formats():
+        # Assume the format can be converted directly to the formats supported by these classes:
+        # AFormat, BFormat
+        # Redefine this `direct_convert_formats` for a concrete format class
+        return []
+        return [ExampleAFormat]
+
+    @classmethod
+    def read(cls, filename: str) -> dict:
+        """Read the data from the file with the `filename` to a dictionary. The dictionary will
+        be used by its corresponding data class."""
+        data_dict = {}
+        data = np.load(filename)
+        data_dict['arr'] = data[0]
+        data_dict['number'] = np.ravel(data[1])[0]
+        return data_dict
+
+    @classmethod
+    def write(cls, object: ExampleData, filename: str, key: str = None):
+        """Save the data with the `filename`."""
+        data_dict = object.get_data()
+        data = np.array([data_dict['arr'], np.zeros_like(data_dict['arr'])])
+        data[1, :] = data_dict['number']
+        np.save(filename, data)
+        if key is not None:
+            return object.from_file(filename, cls, key)
+
+    @classmethod
+    def convert(cls, input: str, output: str, output_format_class: str, key,
+                **kwargs):
+        """Direct convert method, if the default converting would be too slow or not suitable for the output_format"""
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+    data = {'arr': np.zeros((2, 3)), 'number': 1}
+    # Python Dictionary mapping
+    test1 = ExampleData.from_dict(data, key='test1')
+    # Write the Python Dictionary in ExampleBFormat and create the data class object
+    test1_dataB = test1.write('data.npy', ExampleBFormat, key='test1_dataB')
+    # Write the file in ExampleAFormat without creating the data class object
+    test1_dataB.write('data.npz', ExampleAFormat)
+    # Write the file in ExampleAFormat creating the data class object test1_dataA
+    test1_dataA = test1_dataB.write('data.npz',
+                                    ExampleAFormat,
+                                    key='test1_dataA')
+    # Write the file in ExampleBFormat from ExampleAFormat
+    test1_dataA.write('data_convert.npy', ExampleBFormat)
+    # Write the file in ExampleBFormat from ExampleAFormat creating the data class object test1_convert
+    test1_convert = test1_dataA.write('data_convert.npy',
+                                      ExampleBFormat,
+                                      key='test_convert')
+    # print(test1_dataA.get_data())
+    ExampleData.list_formats()
+    print(test1)
+    print(test1_convert)
