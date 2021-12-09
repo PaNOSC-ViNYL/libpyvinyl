f2aa88d08f73134fce1024ba06eca4868dc8067f
Juncheng E <juncheng.e@xfel.eu>
2021-12-09 16:04:37 +0100

    update BaseData and BaseFormat examples

--- libpyvinyl/BaseData.py
+++ libpyvinyl/BaseData.py
@@ -1,44 +1,60 @@
 from abc import abstractmethod
 from libpyvinyl.AbstractBaseClass import AbstractBaseClass
-from importlib import import_module
 
 
 class BaseData(AbstractBaseClass):
+    # It represents a python object
     """The abstract wrapper data class. It's designed in a way so that its inheritance is an API of a
     kind of data (e.g. wavefront, sample, and diffraction) rather than that of a certain data format."""
     @abstractmethod
-    def __init__(self):
-        self._ioformats = {
-            'UNKNOWN': {  # FORMAT KEY
-                'description': 'UNKNOWN data format',  # FORMAT DISCRIPTION
-                'ext': '',  # FORMAT EXTENSION
-                'module': '',  # API MODULE NAME OF THE FORMAT
-                'format_class': '',  # API MODULE NAME OF THE FORMAT
-                'read_kwargs': [''],  # KEYWORDS LIST NEEDED TO READ
-                'write_kwargs': ['']  # KEYWORDS LIST NEEDED TO WRITE
-            }
-        }
+    def __init__(self,
+                 key,
+                 data_dict=None,
+                 filename=None,
+                 file_format_class=None,
+                 file_format_kwargs=None):
+        self.__key = key
+        excepted_data = {}
+        excepted_data['data1'] = 1
+        excepted_data['data2'] = 2
+        self.__expected_data = excepted_data
+        self.__ioformats = {}
+        self._add_ioformat(openPMDFormat)
+        self._add_ioformat(singfelFormat)
+
+        # This will be always be None if the data class is mapped to a file
+        self.__data_dict = data_dict
+        # These will be always be None if the data class is mapped to a python data dict object
+        self.__filename = filename
+        self.__file_format_class = file_format_class
+        self.__file_format_kwargs = file_format_kwargs
+
+    @property
+    def key(self):
+        """The key of the class instance for calculator usage"""
+        return self.__key
 
     @property
     def ioformats(self):
-        return self._ioformats
+        return self.__ioformats
 
-    @staticmethod
-    def _add_ioformat(ioformats,
-                      key: str,
-                      description: str,
-                      ext: str,
-                      module: str,
-                      format_class: str,
-                      read_kwargs=[''],
-                      write_kwargs=['']):
-        ioformats[key] = {}
-        ioformats[key]['description'] = description
-        ioformats[key]['ext'] = ext
-        ioformats[key]['module'] = module
-        ioformats[key]['format_class'] = format_class
-        ioformats[key]['read_kwargs'] = read_kwargs
-        ioformats[key]['write_kwargs'] = write_kwargs
+    @property
+    def mapping_type(self):
+        if self.__data_dict is not None:
+            return 'Python Dictionary'
+        elif self.__filename is not None:
+            return 'Data file: {}'.format(self.__filename)
+        else:
+            raise TypeError(
+                'Niether self.__data_dict or self.__filename was found.')
+
+    def _add_ioformat(self, format_class):
+        for key, val in format_class.items():
+            if key == 'key':
+                this_format = val
+                self.__ioformats[val] = {}
+            else:
+                self.__ioformats[this_format][key] = val
 
     def listFormats(self):
         """Print supported formats"""
@@ -50,9 +66,9 @@ class BaseData(AbstractBaseClass):
             ext = dicts['ext']
             if ext != '':
                 out_string += 'File extension: {}\n'.format(ext)
-            module = dicts['module']
-            if module != '':
-                out_string += 'API module: {}\n'.format(module)
+            format_class = dicts['format_class']
+            if format_class != '':
+                out_string += 'Format class: {}\n'.format(format_class)
             kwargs = dicts['read_kwargs']
             if kwargs != ['']:
                 out_string += 'Extra reading keywords: {}\n'.format(kwargs)
@@ -63,32 +79,86 @@ class BaseData(AbstractBaseClass):
         print(out_string)
 
     @classmethod
-    def read(self, filename: str, format_class, **kwargs):
-        """Read the data from the file with the `filename` in the `format`. It returns an instance of the data class"""
-        # module_name = self._ioformats[format]['module']
-        # format_class = self._ioformats[format]['format_class']
-        # data_class = getattr(import_module(module_name), format_class)
-        ## This should work if read is a classmethod
-        return format_class.read(filename, **kwargs)
+    def from_file(cls, filename: str, format_class, **kwargs):
+        """Create the data class by the file in the `format`."""
+        return cls(filename=filename,
+                   file_format_class=format_class,
+                   file_format_kwargs=kwargs)
+
+    @classmethod
+    def from_dict(cls, data_dict):
+        """Create the data class by a python dictionary."""
+        return cls(data_dict=data_dict)
 
     def write(self, filename: str, format_class, **kwargs):
+        # From either a file or a python object to a file
+        # The behaviour related to a file will always be handled by
+        # the format class.
         """Save the data with the `filename` in the `format`."""
         return format_class.write(self, filename, **kwargs)
 
-    @classmethod
-    def convert(self, input_filename, input_format, output_filename,
-                output_format, **kwargs):
-        """Convert a file into another format"""
-        module_name = self._ioformats[input_format]['module']
-        data_module = import_module(module_name)
-        # If there is a direct converter
-        if data_module.convert(input_filename, output_filename, output_format,
-                               **kwargs):
-            pass
-        else:
-            data_obj = self.read(input_filename, input_format)
-            out_module_name = self._ioformats[input_format]['module']
-            out_data_module = import_module(out_module_name)
-            out_data_module.write(data_obj, output_filename, **kwargs)
+    def get_dict_data(self):
+        # python object to python object
+        if self.__data_dict is not None:
+            data_to_return = self.__expected_data.copy()
+            # It will automatically check the data needed to be extracted.
+            for key in data_to_return.keys():
+                data_to_return[key] = self.__data_dict[key]
+            return data_to_return
+
+    def get_file_data(self):
+        # file to python object
+        if self.__filename is not None:
+            data_to_return = self.__expected_data.copy()
+            data_to_read = self.__file_format_class.read(
+                self.__filename, **self.__file_format_kwargs)
+            # It will automatically check the data needed to be extracted.
+            for key in data_to_return.keys():
+                data_to_return[key] = data_to_read[key]
+            return data_to_return
+
+    def get_data(self):
+        """Return the data in a dictionary"""
+        # From either a file or a python object to a python object
+        if self.__data_dict is not None:
+            return self.get_dict_data()
+        elif self.__filename is not None:
+            return self.get_file_data()
+
+    def __repr__(self):
+        """Returns strings of Data objets info"""
+        string = ''
+        string += self.key + ': ' + self.mapping_type + '\n'
+        return string
 
 
+# DataCollection class
+class DataCollection():
+    def __init__(self, *args):
+        self.data_object_dict = {}
+        self.add_data(*args)
+
+    def __getitem__(self, *keys):
+        subset = []
+        for key in keys:
+            subset.append(self.data_object_dict[key])
+        return DataCollection(*subset)
+
+    def add_data(self, *args):
+        """Add data objects to the data colletion"""
+        for data in args:
+            self.data_object_dict[data.key] = data
+
+    def get_data_object(self, key):
+        return self.data_object_dict[key]
+
+    def to_list(self):
+        """Export a list of the data objects in the data colletion"""
+        return [self.data_object_dict[key] for key in self.data_dict.keys()]
+
+    def __repr__(self):
+        """Returns strings of Data objets info"""
+        string = ''
+        for data_object in self.data_object_dict.values():
+            string += str(data_object)
+        return string
