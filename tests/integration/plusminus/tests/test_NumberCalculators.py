694ffa3cdb84cfff3ba43e490e59e9dc45829d2c
Juncheng E <juncheng.e@xfel.eu>
2021-12-14 11:42:20 +0100

    NumberCalculatorsFinished

--- tests/integration/plusminus/tests/test_NumberCalculators.py
+++ tests/integration/plusminus/tests/test_NumberCalculators.py
@@ -8,37 +8,46 @@ from plusminus import DataCollection
 
 
 def test_PlusCalculator():
-    """PlusCalculator test function"""
+    """PlusCalculator test function, the native output of MinusCalculator is a python dictionary"""
 
     input1 = NumberData.from_dict({'number': 1}, 'input1')
     input2 = NumberData.from_dict({'number': 1}, 'input2')
-    input_data = [input1, input2] # This could also be allowed.
+    input_data = [input1, input2]  # This could also be allowed.
     input_data = DataCollection(input1, input2)
     plus = PlusCalculator('plus', input_data)
     plus_output = plus.backengine()
     print(plus_output.get_data())
-    plus_output.write('1_time.txt', TXTFormat)
+    plus_output.write(plus.base_dir + '/1_time.txt', TXTFormat)
     plus.parameters['plus_times'] = 5
     plus_output = plus.backengine()
-    file_output = plus_output.write('5_time.txt', TXTFormat, key='file_output')
+    file_output = plus_output.write(plus.base_dir + '/5_time.txt',
+                                    TXTFormat,
+                                    key='file_output')
     print(file_output)
+
 
 def test_MinusCalculator():
-    """PlusCalculator test function"""
+    """MinusCalculator test function. The native output of MinusCalculator is a txt file"""
 
     input1 = NumberData.from_dict({'number': 1}, 'input1')
     input2 = NumberData.from_dict({'number': 1}, 'input2')
     input_data = DataCollection(input1, input2)
-    plus = PlusCalculator('plus', input_data)
-    plus_output = plus.backengine()
-    print(plus_output.get_data())
-    plus_output.write('1_time.txt', TXTFormat)
-    plus.parameters['plus_times'] = 5
-    plus_output = plus.backengine()
-    file_output = plus_output.write('5_time.txt', TXTFormat, key='file_output')
+    calculator = MinusCalculator('minus', input_data)
+    print(calculator.base_dir)
+    calculator.set_output_filenames('minus_res.txt')
+    output = calculator.backengine()
+    print(output.get_data())
+    output.write(calculator.base_dir + '/1_time_minus.txt', TXTFormat)
+    calculator.parameters['minus_times'] = 5
+    plus_output = calculator.backengine()
+    file_output = plus_output.write(calculator.base_dir + '/5_time_minus.txt',
+                                    TXTFormat,
+                                    key='file_output')
     print(file_output)
 
+
 # def test_Instrument():
+
 
 def test_DataCollection_multiple():
     """PlusCalculator test function"""
