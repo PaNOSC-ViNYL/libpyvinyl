e0e74fa7247aa188ec6c41a59ba615333302ecce
Juncheng E <juncheng.e@xfel.eu>
2022-01-13 21:08:44 +0100

    Black format

--- tests/integration/plusminus/plusminus/ArrayData/H5Format.py
+++ tests/integration/plusminus/plusminus/ArrayData/H5Format.py
@@ -9,13 +9,14 @@ class H5Format(BaseFormat):
 
     @classmethod
     def format_register(self):
-        key = 'H5'
-        desciption = 'H5 format for ArrayData'
-        file_extension = '.h5'
-        read_kwargs = ['']
-        write_kwargs = ['']
-        return self._create_format_register(key, desciption, file_extension,
-                                            read_kwargs, write_kwargs)
+        key = "H5"
+        desciption = "H5 format for ArrayData"
+        file_extension = ".h5"
+        read_kwargs = [""]
+        write_kwargs = [""]
+        return self._create_format_register(
+            key, desciption, file_extension, read_kwargs, write_kwargs
+        )
 
     @staticmethod
     def direct_convert_formats():
@@ -28,21 +29,21 @@ class H5Format(BaseFormat):
     def read(cls, filename: str) -> dict:
         """Read the data from the file with the `filename` to a dictionary. The dictionary will
         be used by its corresponding data class."""
-        with h5py.File(filename, 'r') as h5:
-            array = h5['array'][()]
-        data_dict = {'array': array}
+        with h5py.File(filename, "r") as h5:
+            array = h5["array"][()]
+        data_dict = {"array": array}
         return data_dict
 
     @classmethod
     def write(cls, object: ArrayData, filename: str, key: str = None):
         """Save the data with the `filename`."""
         data_dict = object.get_data()
-        array = data_dict['array']
-        with h5py.File(filename, 'w') as h5:
-            h5['array'] = array
+        array = data_dict["array"]
+        with h5py.File(filename, "w") as h5:
+            h5["array"] = array
         if key is None:
             original_key = object.key
-            key = original_key + '_to_H5Format'
+            key = original_key + "_to_H5Format"
             return object.from_file(filename, cls, key)
         else:
             return object.from_file(filename, cls, key)
--- tests/integration/plusminus/setup.py
+++ tests/integration/plusminus/setup.py
@@ -4,46 +4,50 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-with open('requirements.txt') as requirements_file:
+with open("requirements.txt") as requirements_file:
     require = requirements_file.read()
     requirements = require.split()
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Juncheng E",
-    author_email='juncheng.e@xfel.eu',
-    python_requires='>=3.6',
+    author_email="juncheng.e@xfel.eu",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="An example of a small platform implementing libpynyl",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='PlusMinus',
-    name='PlusMinus',
-    packages=find_packages(include=['plusminus', 'plusminus.*']),
+    keywords="PlusMinus",
+    name="PlusMinus",
+    packages=find_packages(include=["plusminus", "plusminus.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/JunCEEE/PlusMinus',
-    version='0.1.0',
+    url="https://github.com/JunCEEE/PlusMinus",
+    version="0.1.0",
     zip_safe=False,
 )
