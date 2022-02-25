e1b34f3bb6e2178be673a2da11615e5c1f1fd427
Juncheng E <juncheng.e@xfel.eu>
2022-02-25 18:05:03 +0100

    Fix #50 by increasing ljust and adding a space
    To deal with very long parameter names, we increasted the ljust and
    added a space after the name, value and unit.

--- libpyvinyl/Parameters/Parameter.py
+++ libpyvinyl/Parameters/Parameter.py
@@ -424,12 +424,12 @@ class Parameter(AbstractBaseClass):
             unit_string = "[" + str(self.__unit) + "] "
 
         if self.value_no_conversion is None:
-            string = self.name.ljust(20)
+            string = self.name.ljust(40) + " "
         else:
-            string = self.name.ljust(15)
-            string += str(self.value).ljust(5)
+            string = self.name.ljust(35) + " "
+            string += str(self.value).ljust(10) + " "
 
-        string += unit_string.ljust(10)
+        string += unit_string.ljust(20) + " "
         if self.comment is not None:
             string += self.comment
         string += 3 * " "
