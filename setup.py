81c8d074a0f4793311fd0f0ad25eb9ac965eb00a
Juncheng E <juncheng.e@xfel.eu>
2022-03-21 10:21:03 +0100

    bump to v1.1.0

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open("requirements/prod.txt") as requirements_file:
 setup(
     name="libpyvinyl",
     packages=find_packages(include=["libpyvinyl", "libpyvinyl.*"]),
-    version="0.1.0",
+    version="1.1.0",
     license="LGPLv3",
     description="The python API for photon and neutron simulation codes in the Photon and Neutron Open Science Cloud (PaNOSC).",
     author="Carsten Fortmann-Grote, Juncheng E, Mads Bertelsen, Shervin Nourbakhsh",
     author_email="carsten.grote@xfel.eu, juncheng.e@xfel.eu, Mads.Bertelsen@ess.eu, nourbakhsh@ill.fr",
     url="https://github.com/PaNOSC-ViNYL/libpyvinyl",
-    download_url="https://github.com/PaNOSC-ViNYL/libpyvinyl/archive/v0.1.0.tar.gz",
+    download_url="https://github.com/PaNOSC-ViNYL/libpyvinyl/archive/v1.1.0.tar.gz",
     keywords=["photons", "neutrons", "simulations"],
     install_requires=requirements,
     classifiers=[
